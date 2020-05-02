8192999b1f68c85ef275361768d7136b10a5e1d2
Paras <sivssdn@gmail.com>
2020-05-03 01:28:44 +0530

    loading image function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 extern crate piston_window;
+extern crate find_folder;
 
 use piston_window::*;
 
 fn main() {
+    let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = 
-        WindowSettings::new("Hello World!", [1024, 720]).exit_on_esc(true).build().unwrap();
+        WindowSettings::new("Hello World!", [1024, 720])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+    
+    let assets = find_folder::Search::ParentsThenKids(3,3)
+                .for_folder("assets").unwrap();
+    let background = Texture::from_path(
+            &mut window.create_texture_context(),
+            assets.join("wallpaper.jpeg"),
+            Flip::None,
+            &TextureSettings::new()
+        ).unwrap();
+    window.set_lazy(true);
+
     while let Some(event) = window.next() {
            window.draw_2d(&event, |context, graphics, _device| {
             clear([1.0; 4], graphics);
@@ -12,6 +29,8 @@ fn main() {
                       [0.0, 0.0, 100.0, 100.0],
                       context.transform,
                       graphics);
+            image(&background, context.transform, graphics)
             });
+            
     }
 }
